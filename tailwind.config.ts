3c20eee87d2848cce2dd8881f3c0c90f232efc8a
ArnaudGct <arnaud.graciet@gmail.com>
2024-12-14 10:08:43 +0100

    settings project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,28 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        "host-grotesk": ["var(--font-host-grotesk)", "sans-serif"],
+        outfit: ["var(--font-outfit)", "sans-serif"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: {
+          50: "var(--blue-ribbon-50)",
+          100: "var(--blue-ribbon-100)",
+          200: "var(--blue-ribbon-200)",
+          300: "var(--blue-ribbon-300)",
+          400: "var(--blue-ribbon-400)",
+          500: "var(--blue-ribbon-500)",
+          600: "var(--blue-ribbon-600)",
+          700: "var(--blue-ribbon-700)",
+          800: "var(--blue-ribbon-800)",
+          900: "var(--blue-ribbon-900)",
+          950: "var(--blue-ribbon-950)",
+        },
       },
     },
   },
