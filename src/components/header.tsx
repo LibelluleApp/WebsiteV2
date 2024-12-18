26aceb88c90347cb744179186a5cbade4478c18e
ArnaudGct <arnaud.graciet@gmail.com>
2024-12-18 19:34:10 +0100

    remove onclick logo libellule

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -75,11 +75,7 @@ export default function Header() {
       {/* Version mobile */}
       <div className="md:hidden relative">
         <div className="flex justify-between items-center px-8 py-4 z-50">
-          <Link
-            href="/"
-            className="flex items-center gap-[9px] z-50"
-            onClick={toggleMenu}
-          >
+          <Link href="/" className="flex items-center gap-[9px] z-50">
             <Image src="/logo.svg" alt="Logo" width={16} height={21} />
             <h1 className="text-xl font-black font-ubuntu text-blue-700 tracking-tight-custom">
               Libellule
