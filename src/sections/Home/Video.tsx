0e9b757834992e533c034510e8eecc962bbf32c0
Raphaël Tiphonet <43700748+Raphitpt@users.noreply.github.com>
2025-05-21 13:03:54 +0200

    Update Video.tsx

--- src/sections/Home/Video.tsx
+++ src/sections/Home/Video.tsx
@@ -1,21 +1,9 @@
 "use client";
 
 import TitleSection from "@/src/components/titleSection";
-// import Plyr from "plyr-react";
-// import "plyr/dist/plyr.css";
+import { YouTubeEmbed } from "@next/third-parties/google";
 
 export default function VideoSection() {
-  // learn more https://github.com/sampotts/plyr#the-source-setter
-  // const videoSrc: Plyr.SourceInfo = {
-  //   type: "video",
-  //   sources: [
-  //     {
-  //       src: "yWtFb9LJs3o",
-  //       provider: "youtube",
-  //     },
-  //   ],
-  // };
-
   return (
     <section className="flex flex-col gap-10 w-[90%] mx-auto max-w-9xl">
       <TitleSection
@@ -33,31 +21,26 @@ export default function VideoSection() {
       />
       <div className="grid gap-4">
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 md:grid-cols-2">
-          {/* Vidéo principale - prend 2 colonnes en desktop */}
           <div className="h-[350px] md:h-[450px] lg:col-span-2 md:col-span-2">
-            <iframe
-              src="https://www.youtube.com/embed/30EjVuj1e7M"
+            <YouTubeEmbed
+              videoid="30EjVuj1e7M"
+              params="controls=1"
               className="w-full h-full rounded-xl"
-              allowFullScreen
-              loading="lazy"
             />
           </div>
 
-          {/* Vidéos verticales - chacune prend 1 colonne en desktop */}
           <div className="h-[700px] lg:h-[450px] md:h-[800px]">
-            <iframe
-              src="https://www.youtube.com/embed/ULzIpc4vo6M"
+            <YouTubeEmbed
+              videoid="ULzIpc4vo6M"
+              params="controls=1"
               className="w-full h-full rounded-xl"
-              allowFullScreen
-              loading="lazy"
             />
           </div>
           <div className="h-[700px] lg:h-[450px] md:h-[800px]">
-            <iframe
-              src="https://www.youtube.com/embed/V4LlmDt5jPY"
+            <YouTubeEmbed
+              videoid="V4LlmDt5jPY"
+              params="controls=1"
               className="w-full h-full rounded-xl"
-              allowFullScreen
-              loading="lazy"
             />
           </div>
         </div>
